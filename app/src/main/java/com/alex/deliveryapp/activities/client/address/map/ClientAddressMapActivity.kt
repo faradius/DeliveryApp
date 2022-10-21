990c1f5d42a1fc667090b742052c7e5c018e7b66
Faradius <faradius.magic187@gmail.com>
2022-10-21 10:04:34 -0500

    Implementando la ubicación actual en el google maps

--- app/src/main/java/com/alex/deliveryapp/activities/client/address/map/ClientAddressMapActivity.kt
+++ app/src/main/java/com/alex/deliveryapp/activities/client/address/map/ClientAddressMapActivity.kt
@@ -1,15 +1,40 @@
 package com.alex.deliveryapp.activities.client.address.map
 
+import android.Manifest
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Looper
+import android.provider.Settings
+import android.util.Log
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import com.alex.deliveryapp.R
+import com.google.android.gms.location.*
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
 
 class ClientAddressMapActivity : AppCompatActivity(), OnMapReadyCallback {
 
     var googleMap: GoogleMap? = null
+
+    val PERMISSION_ID = 42
+    var fusedLocationClient: FusedLocationProviderClient? = null
+
+    private val locationCallback = object : LocationCallback(){
+        override fun onLocationResult(locationResult: LocationResult) {
+            //Con esto podemos ver la localización en la que nos encontramos
+            var lastLocation = locationResult.lastLocation
+            Log.d("LOCALIZACION", "Callback: $lastLocation ")
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,9 +43,106 @@ class ClientAddressMapActivity : AppCompatActivity(), OnMapReadyCallback {
         val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as? SupportMapFragment
         mapFragment?.getMapAsync(this)
 
+        //Esta variable nos va ayudar a iniciar el servicio para encontrar nuestra localización
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
+        getLastLocation()
+
     }
 
     override fun onMapReady(map: GoogleMap) {
         googleMap = map
+    }
+
+    private fun getLastLocation(){
+        //Primero verificamos los permisos
+        if(checkPermission()){
+            if (isLocationEnabled()){
+                fusedLocationClient?.lastLocation?.addOnCompleteListener {  task ->
+
+                    var location = task.result
+                    if (location == null){
+                        requestNewLocationData()
+                    }else{
+                        googleMap?.moveCamera(CameraUpdateFactory.newCameraPosition(
+                            CameraPosition.builder().target(
+                                LatLng(location.latitude, location.longitude)
+                            ).zoom(15f).build()
+                        ))
+                    }
+                }
+            }else{
+                Toast.makeText(this, "Habilita la localización", Toast.LENGTH_LONG).show()
+                val i = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+                startActivity(i)
+            }
+
+        }else{ //Si no solicitamos los permisos
+            requestPermissions()
+        }
+    }
+
+    //Son configuraciones y se puede reutilizar en otro proyecto
+    private fun requestNewLocationData(){
+        val locationRequest = LocationRequest.create().apply {
+            interval = 100
+            fastestInterval = 50
+            priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        }
+
+        //Este es el que empezará a escuchar la localización en tiempo real
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            return
+        }
+        fusedLocationClient?.requestLocationUpdates(locationRequest, locationCallback, Looper.myLooper())
+    }
+
+    private fun isLocationEnabled():Boolean{
+        var locationManager: LocationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(
+            LocationManager.NETWORK_PROVIDER
+        )
+    }
+
+    private fun checkPermission(): Boolean {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+            == PackageManager.PERMISSION_GRANTED
+            && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+            == PackageManager.PERMISSION_GRANTED
+        ) {
+            return true
+        }
+        return false
+    }
+
+    private fun requestPermissions() {
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ),
+            PERMISSION_ID
+        )
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == PERMISSION_ID){
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                getLastLocation()
+            }
+        }
     }
 }
