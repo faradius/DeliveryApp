5dc3c78531802792f3e3ebbf36edcb540627243c
Alejandro <faradius.magic187@gmail.com>
2022-11-24 17:05:08 -0600

    Enviando notificaciones push de dispositivo a dispositivo

--- app/src/main/java/com/alex/deliveryapp/services/MyFirebaseMessagingClient.kt
+++ app/src/main/java/com/alex/deliveryapp/services/MyFirebaseMessagingClient.kt
@@ -1,7 +1,9 @@
 package com.alex.deliveryapp.services
 
+import com.alex.deliveryapp.channel.NotificationHelper
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
+import com.google.firebase.messaging.ktx.remoteMessage
 
 class MyFirebaseMessagingClient: FirebaseMessagingService() {
 
@@ -9,7 +11,24 @@ class MyFirebaseMessagingClient: FirebaseMessagingService() {
         super.onNewToken(token)
     }
 
-    override fun onMessageReceived(message: RemoteMessage) {
-        super.onMessageReceived(message)
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        super.onMessageReceived(remoteMessage)
+
+        //Es un mapa de valores
+        val data = remoteMessage.data
+        val title = data["title"]
+        val body = data["body"]
+        val idNotification = data["id_notification"]
+
+        if (!title.isNullOrBlank() && !body.isNullOrBlank() && !idNotification.isNullOrBlank()){
+            showNotification(title,body,idNotification)
+        }
+    }
+
+    private fun showNotification(title:String, body:String, idNotification:String){
+        val helper = NotificationHelper(baseContext)
+        val builder = helper.getNotification(title, body)
+        val id = idNotification.toInt()
+        helper.getManager().notify(id, builder.build())
     }
 }
